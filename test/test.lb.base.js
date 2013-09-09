0c54e0c8c7584bd0a4d61388513b41cccaf12d4f
Eric Bréchemier <eric@bezen.org>
2013-09-09 10:59:04 +0200

    Add unit tests for no(), copied from nada (CC0)
    nada project on GitHub:
    https://github.com/eric-brechemier/nada

--- test/test.lb.base.js
+++ test/test.lb.base.js
@@ -1,16 +1,16 @@
 /*
- * test.lb.base.js - Unit Tests of lb.base namespace
+ * test.lb.base.js - Unit Tests of lb.base module
  *
  * Author:    Eric Bréchemier <github@eric.brechemier.name>
  * Copyright:
- * Eric Bréchemier (c) 2011, Some Rights Reserved.
+ * Eric Bréchemier (c) 2011-2013, Some Rights Reserved.
  * Legal-Box (c) 2010-2011, All Rights Reserved.
  *
  * License:
  * BSD License
  * http://creativecommons.org/licenses/BSD/
  *
- * Version:   2011-08-14
+ * Version:   2013-09-09
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
@@ -45,8 +45,35 @@ define(
       }
     }
 
+    function testNo(){
+      var no = base.no;
+
+      assert( no( null ),            "TEST1: no() must return true for null" );
+      assert( no( undefined ),  "TEST1: no() must return true for undefined" );
+
+      /*
+        Check that no() returns false for values different from null and undefined
+      */
+      function checkFalseFor( value ) {
+        var result = no( value );
+        assert( result === false,
+                          "TEST2: no() must return false for other values ; " +
+                                 "found: " + result + " for '" + value + "'" );
+      }
+
+      checkFalseFor( false );
+      checkFalseFor( {} );
+      checkFalseFor( "" );
+      checkFalseFor( "abc" );
+      checkFalseFor( [] );
+      checkFalseFor( [ 1, 2, 3 ] );
+      checkFalseFor( 0 );
+      checkFalseFor( 42 );
+    }
+
     var tests = {
-      testNamespace: testNamespace
+      testNamespace: testNamespace,
+      testNo: testNo
     };
 
     testrunner.define(tests, "lb.base");
