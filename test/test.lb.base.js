a3670eac3ac7f61a7629f3a3d89c12007c0401dd
Eric Bréchemier <eric@bezen.org>
2013-09-10 11:18:57 +0200

    Add unit tests for or(), copied from nadasurf (CC0)
    nadasurf project on GitHub:
    https://github.com/eric-brechemier/nadasurf

--- test/test.lb.base.js
+++ test/test.lb.base.js
@@ -10,7 +10,7 @@
  * BSD License
  * http://creativecommons.org/licenses/BSD/
  *
- * Version:   2013-09-09
+ * Version:   2013-09-10
  *
  * Based on Test Runner from bezen.org JavaScript library
  * CC-BY: Eric Bréchemier - http://bezen.org/javascript/
@@ -72,9 +72,81 @@ define(
       checkFalseFor( 42 );
     }
 
+    function testOr(){
+      var or = base.or;
+
+      assert.isTrue(
+        or( true, false ) === true &&
+        or( false, true ) === false,
+                        "first boolean value provided is expected as result" );
+      assert.isTrue(
+        or( 0, 1 ) === 0 &&
+        or( 1, 0 ) === 1,
+                               "first number provided is expected as result" );
+
+      assert.isTrue(
+        or( "a", "b" ) === "a" &&
+        or( "b", "a" ) === "b",
+                         "first string value provided is expected as result" );
+
+      var
+        objectA = {},
+        objectB = new Date();
+
+      assert.isTrue(
+        or( objectA, objectB ) === objectA &&
+        or( objectB, objectA ) === objectB,
+                               "first object provided is expected as result" );
+
+      var
+        arrayA = [],
+        arrayB = [ 1, "two", /3/ ];
+
+      assert.isTrue(
+        or( arrayA, arrayB ) === arrayA &&
+        or( arrayB, arrayA ) === arrayB,
+                                "first array provided is expected as result" );
+
+      function funcA() {}
+      function funcB() { return 42; }
+
+      assert.isTrue(
+        or( funcA, funcB ) === funcA &&
+        or( funcB, funcA ) === funcB,
+                             "first function provided is expected as result" );
+
+      assert.isTrue(
+        or( null, undefined ) === undefined &&
+        or( undefined, null ) === null,
+                        "last null or undefined value is expected as result" );
+
+      assert.isTrue(
+        or( null, false ) === false &&
+        or( null, true ) === true &&
+        or( null, 0 ) === 0 &&
+        or( null, 1 ) === 1 &&
+        or( null, "a" ) === "a" &&
+        or( null, objectA ) === objectA &&
+        or( null, arrayA ) === arrayA &&
+        or( null, funcA ) === funcA,
+                   "second argument is expected when first argument is null" );
+
+      assert.isTrue(
+        or( undefined, false ) === false &&
+        or( undefined, true ) === true &&
+        or( undefined, 0 ) === 0 &&
+        or( undefined, 1 ) === 1 &&
+        or( undefined, "a" ) === "a" &&
+        or( undefined, objectA ) === objectA &&
+        or( undefined, arrayA ) === arrayA &&
+        or( undefined, funcA ) === funcA,
+              "second argument is expected when first argument is undefined" );
+    }
+
     var tests = {
       testNamespace: testNamespace,
-      testNo: testNo
+      testNo: testNo,
+      testOr: testOr
     };
 
     testrunner.define(tests, "lb.base");
